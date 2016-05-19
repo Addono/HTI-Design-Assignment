2e543e5e9fec1aab1a15fea8c4e674d30a2f5361
Addono <a.d.knapen@student.tue.nl>
2016-05-19 20:18:43 +0200

    Worked on design.

--- js/page-transitions.js
+++ js/page-transitions.js
@@ -103,4 +103,27 @@ $(document).ready(function(){
     $("header").css("height", hHeader);
     
     initial();
+    
+    startTime();
 });
+
+
+function startTime() {
+    var today = new Date();
+    var y = today.getFullYear();
+    var M = today.getMonth() + 1;
+    var d = today.getDate();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('dateTime').innerHTML = 
+            M + "/" + d + "/" + y + " " + h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
